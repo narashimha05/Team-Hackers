aaceff95e1321bed8bd890680492e78b80a82319
Ashwani Senapati <senapatiashwani47@gmail.com>
2024-09-11 00:36:35 +0530

    feat:faq

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Faq from "./components/Faq/Faq";
+import Carousel from "./components/Carousel/Carousel"
 function App() {
   return (
     <div>
       <h1 className="text-3xl">Team Hackers</h1>
-     
+      <Carousel/>
+      <Faq/>
     </div>
   );
 }
