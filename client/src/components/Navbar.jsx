a1abf5e251979726f996f605f170dce3c6342300
Payaldas <payaldaskunu30@gmail.com>
2024-09-10 23:52:02 +0530

    feat:Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+"use client";
+import React, { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { ImCross } from "react-icons/im";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div id="home">
+      <nav className="font-poppins">
+        <div className="sm:block md:hidden ">
+          {!isOpen ? (
+            <button
+              onClick={() => setIsOpen(true)}
+              className="flex mt-4 px-3 mx-2 py-2 border rounded  text-black border-gray-400 "
+            >
+              <FiMenu />
+            </button>
+          ) : (
+            <button
+              onClick={() => setIsOpen(false)}
+              className="flex mt-4 px-3 mx-2 py-2 border rounded text-red-600 border-red-500"
+            >
+              <ImCross />
+            </button>
+          )}
+
+          {isOpen && (
+            <ul className=" mt-5 md:hidden bg-blue-950 grid grid-rows-4 pt-10 text-center">
+              <li className=" mb-10">
+                <a
+                  className="p-4 rounded-2xl  hover:font-bold text-white font-medium font-poppins"
+                  href="#home"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  className="p-4 rounded-2xl hover:font-bold text-white font-medium font-serif"
+                  href="#about"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  className="p-4 rounded-2xl hover:font-bold text-white font-medium font-serif"
+                  href="#services"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  className="p-4 rounded-2xl hover:font-bold text-white font-medium font-serif"
+                  href="#contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          )}
+        </div>
+
+        <ul className="hidden md:flex gap-16 bg-blue-950 justify-end text-center p-5">
+  <li>
+    <a
+      className="p-4 rounded-2xl hover:font-bold text-white font-medium font-poppins "
+      href="#home"
+    >
+      Home
+    </a>
+  </li>
+  <li>
+    <a
+      className="p-4 rounded-2xl hover:font-bold text-white font-medium font-poppins"
+      href="#about"
+    >
+      About
+    </a>
+  </li>
+  <li>
+    <a
+      className="p-4 rounded-2xl hover:font-bold text-white font-medium font-poppins"
+      href="#services"
+    >
+      Services
+    </a>
+  </li>
+  <li>
+    <a
+      className="p-4 rounded-2xl hover:font-bold text-white font-medium font-poppins "
+      href="#contact"
+    >
+      Contact
+    </a>
+  </li>
+</ul>
+
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
